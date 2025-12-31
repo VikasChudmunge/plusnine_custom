624495cadb3d1ff59e3673f798282bb17db50b19
VikasChudmunge <vikaschudmunge5901@gmail.com>
2025-12-31 18:34:09 +0530

    b

--- plusnine_custom/hooks.py
+++ plusnine_custom/hooks.py
@@ -7,7 +7,7 @@ app_license = "mit"
 
 # Apps
 # ------------------
-# stunted apps will not be installed until all their dependencies are installed
+# stunted apps will not be installed until all thei
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
